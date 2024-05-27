c35702f3377d23b1b171f945830de23ae3bbb1db
pro-masud <promasudbd@gmail.com>
2024-05-28 00:13:12 +0600

    React JS useEffect Hook Working Successfully

--- src/pages/Team/Team.jsx
+++ src/pages/Team/Team.jsx
@@ -1,10 +1,27 @@
-import React from 'react';
+import { useEffect, useState } from 'react';
 import './team.scss';
 
 const Team = () => {
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    console.log("My name is masud rana")
+  }, [count]);
+
+
+  const setCountHandleINC = () => {
+     setCount((prive) => prive + 1)
+  }
+  const setCountHandleDSCE = () => {
+     setCount((prive) => prive - 1)
+  }
   return (
     <>
-      <h1>Team Member</h1>
+     <div className="container">
+      <h1>{count}</h1>
+      <button onClick={setCountHandleINC}>++</button>
+      <button onClick={setCountHandleDSCE}>--</button>
+     </div>
     </>
   )
 }
